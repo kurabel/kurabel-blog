e71f90e3b413f5b6f6493b953e59b008d5484a65
k.soma <>
2018-11-26 14:26:50 +0900

    init コメント

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="index">
+    <div
+      class="title is-4"
+      v-for="(item, index) in items" :key="index">
+        <img :src="item.fields.image[0].fields.file.url" width="200">
+        <h1>{{ item.fields.name }}</h1>
+        <b>{{ item.fields.price }}</b>
+    </div>
+  </section>
+</template>
+
+<script>
+import client from '~/plugins/contentful';
+
+export default {
+  asyncData({ params }) {
+    return client.getEntries()
+      .then(entries => {
+        return { items: entries.items };
+      })
+      .catch(console.error);
+  }
+};
+</script>
---
+++ pages/item/_id.vue
@@ -0,0 +1,10 @@
+<template>
+	<section>
+    itemId:{{ $route.params.id }}
+    <h1>商品名</h1>
+    <data><small>￥</small>10,000</data>
+    <div>
+      このテキストは商品詳細に表示するダミーテキストです。このテキストは商品詳細に表示するダミーテキストです。このテキストは商品詳細に表示するダミーテキストです。
+    </div>
+  </section>
+</template>
