2150fd921445ee031e1911ece66f1f018f7a0245
k.soma <>
2018-12-07 18:23:44 +0900

    fix

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -4,3 +4,7 @@ module.exports = contentful.createClient({
     space: process.env.CTF_SPACE_ID,
     accessToken: process.env.CTF_ACCESS_TOKEN
 })
+
+console.log('--CTF_ACCESS_TOKEN---')
+console.log(process.env.CTF_ACCESS_TOKEN)
+console.log('---------------')
